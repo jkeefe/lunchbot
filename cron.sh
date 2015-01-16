3bf94895055d60310e33510fa19b4604432a5e53
John Keefe <john@johnkeefe.net>
2015-01-15 20:43:23 -0500

    comment cron.sh

--- cron.sh
+++ cron.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-## use the following as an example of using python environment stuff
+# Tell cron to use my local .profile information
 source /home/ubuntu/.profile
 
+# change directories into the /lunchbot
 cd /home/ubuntu/bothouse/lunchbot
 
+# run the bot itself using node
 node lunchbot.js
